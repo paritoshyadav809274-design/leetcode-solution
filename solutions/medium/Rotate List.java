aa16b8d31d1e988f397e61b29ec263777b203a70
paritosh anand <paritoshyadav809274@gmail.com>
2026-07-01 01:26:50 +0530

    Add solution for Rotate List

---
+++ solutions/medium/Rotate List.java
@@ -0,0 +1,25 @@
+// Title: Rotate List
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/rotate-list/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null || k == 0) {
+       return head;
+}
+ListNode tail = head;
+int length = 1;
+while (tail.next != null) {
+tail = tail.next;
+length++;
