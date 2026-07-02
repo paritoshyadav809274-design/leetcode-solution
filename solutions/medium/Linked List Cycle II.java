9d14869b41a6e5b05a789c7fd514301be0679812
Paritosh Anand <paritoshyadav809274@gmail.com>
2026-07-02 21:27:19 +0530

    Add solution for Linked List Cycle II

---
+++ solutions/medium/Linked List Cycle II.java
@@ -0,0 +1,26 @@
+// Title: Linked List Cycle II
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/linked-list-cycle-ii/
+
+        fast = fast.next.next;
+        if (slow == fast) {
+        slow = slow.next;
+            break;
+    while (fast != null && fast.next != null) {
+        }
+    }
+    if (fast == null || fast.next == null) {
+        return null;
+    }
+    slow = head;
+    while (slow != fast) {
+    ListNode fast = head;
+    ListNode slow = head;
+    public ListNode detectCycle(ListNode head) {
+public class Solution {
+ */
+ * }
+ *     }
+ *         next = null;
+ *         val = x;
