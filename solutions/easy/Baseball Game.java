1cd600ca4d1206f65204c274c884074beaa0c775
Paritosh Anand <paritoshyadav809274@gmail.com>
2026-07-11 23:47:48 +0530

    Add solution for Baseball Game

--- solutions/easy/Baseball Game.java
+++ solutions/easy/Baseball Game.java
@@ -3,24 +3,24 @@
             // Language: Java
             // Link: https://leetcode.com/problems/baseball-game/
 
-                int b=stack.peek();
+                int a = stack.pop();
+                int b = stack.peek();
                 stack.push(a);
-                stack.push(a+b);
-            }else if(op.equals("D")){
-                stack.push(stack.peek()*2);
-            }else if(op.equals("C")){
+                stack.push(a + b);
+            } else if (op.equals("D")) {
+                stack.push(stack.peek() * 2);
+            } else if (op.equals("C")) {
                 stack.pop();
-            }else{
+            } else {
                 stack.push(Integer.parseInt(op));
             }
         }
-        int sum=0;
-        while(!stack.isEmpty()){
-            sum+=stack.pop();
-            if(op.equals("+")){
-                int a=stack.pop();
-        for(String op:operations){
-    public int calPoints(String[] operations) {
-        Stack<Integer> stack=new Stack<>();
+
+        for (String op : operations) {
+            if (op.equals("+")) {
+        Stack<Integer> stack = new Stack<>();
+
 import java.util.Stack;
+
 class Solution {
+    public int calPoints(String[] operations) {
