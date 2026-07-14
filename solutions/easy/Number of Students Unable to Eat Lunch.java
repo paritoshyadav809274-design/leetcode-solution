985a5633ed7abce7d7cedd8f99d80f5859ae54b9
Paritosh Anand <paritoshyadav809274@gmail.com>
2026-07-14 14:27:03 +0530

    Add solution for Number of Students Unable to Eat Lunch

---
+++ solutions/easy/Number of Students Unable to Eat Lunch.java
@@ -0,0 +1,26 @@
+// Title: Number of Students Unable to Eat Lunch
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/number-of-students-unable-to-eat-lunch/
+
+                one++;
+            }
+        }
+
+        for (int sandwich : sandwiches) {
+            if (sandwich == 0) {
+                if (zero == 0) {
+                    break;
+                }
+                zero--;
+            } else {
+                if (one == 0) {
+            } else {
+                zero++;
+            if (student == 0) {
+        for (int student : students) {
+
+        int zero = 0;
+        int one = 0;
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
