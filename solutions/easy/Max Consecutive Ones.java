a11b821e453ff4a29be6043d665d12d51865d05a
Paritosh Anand <paritoshyadav809274@gmail.com>
2026-07-20 18:05:21 +0530

    Add solution for Max Consecutive Ones

---
+++ solutions/easy/Max Consecutive Ones.java
@@ -0,0 +1,22 @@
+// Title: Max Consecutive Ones
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/max-consecutive-ones/
+
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count=0;
+      for(int i=0;i<nums.length;i++){
+        if(nums[i]==1){
+            count++;
+        }
+      }
+        else {
+        int max=0;
+            
+        }
+    }
+            count=0;
+      return Math.max(max,count);  
+         max=   Math.max(max,count);  
+}
