1a522d25ecc3c2cc92fd5d12483a4467fc8c8623
Paritosh Anand <paritoshyadav809274@gmail.com>
2026-07-24 23:49:37 +0530

    Add solution for Merge Sorted Array

--- solutions/easy/Merge Sorted Array.java
+++ solutions/easy/Merge Sorted Array.java
@@ -3,9 +3,6 @@
             // Language: Java
             // Link: https://leetcode.com/problems/merge-sorted-array/
 
-        int j = n - 1;
-        int k = m + n - 1;
-
         while (i >= 0 && j >= 0) {
             if (nums1[i] > nums2[j]) {
                 nums1[k] = nums1[i];
@@ -21,6 +18,9 @@
             nums1[k] = nums2[j];
             j--;
             k--;
+
+        int k = m + n - 1;
         int i = m - 1;
+        int j = n - 1;
     public void merge(int[] nums1, int m, int[] nums2, int n) {
 class Solution {
