e377c691508d05bf8fbbe47be1619d74ef46c1b1
Paritosh Anand <paritoshyadav809274@gmail.com>
2026-07-30 21:04:00 +0530

    Add solution for Sort Colors

--- solutions/medium/Sort Colors.java
+++ solutions/medium/Sort Colors.java
@@ -3,24 +3,21 @@
             // Language: Java
             // Link: https://leetcode.com/problems/sort-colors/
 
-
-        while (mid <= high) {
-            if (nums[mid] == 0) {
-                int temp = nums[low];
-                nums[low] = nums[mid];
-                nums[mid] = temp;
-                low++;
-                mid++;
-            } 
-            else if (nums[mid] == 1) {
-                mid++;
-            } 
-            else {
-                int temp = nums[mid];
-                nums[mid] = nums[high];
-                nums[high] = temp;
-        int mid = 0;
-        int high = nums.length - 1;
+        }
+        else if(nums[left]==2){
+           int temp=nums[left];
+            nums[left]=nums[right];
+            nums[right]=temp;
+            zero++;
+        right--;
+            left++;
+            nums[zero]=temp;
+            nums[left]=nums[zero];
+            int temp=nums[left];
+     while(left<=right){
+        if(nums[left]==0){
+     int zero=0;
+     int right=nums.length-1;
+     int left=0;
     public void sortColors(int[] nums) {
-        int low = 0;
 class Solution {
