4597d51cd34df970437454bc998549d91bcbe239
Paritosh Anand <paritoshyadav809274@gmail.com>
2026-07-31 22:40:08 +0530

    Add solution for Rearrange Array Elements by Sign

---
+++ solutions/medium/Rearrange Array Elements by Sign.java
@@ -0,0 +1,21 @@
+// Title: Rearrange Array Elements by Sign
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/rearrange-array-elements-by-sign/
+
+       }
+            array[pos]=ele;
+        }
+            pos+=2;
+        else{
+            array[neg]=ele;
+        }
+            neg+=2;
+       return array;
+       int neg=1;
+       for(int ele:nums){
+        if(ele>0){
+       int pos=0;
+    public int[] rearrangeArray(int[] nums) {
+       int [] array=new int[nums.length];
+class Solution {
