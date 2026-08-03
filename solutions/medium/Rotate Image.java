f0b37eb7efc7ed3f62f7b6a4310eb9c1cf5f4faa
Paritosh Anand <paritoshyadav809274@gmail.com>
2026-08-03 18:43:06 +0530

    Add solution for Rotate Image

---
+++ solutions/medium/Rotate Image.java
@@ -0,0 +1,23 @@
+// Title: Rotate Image
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/rotate-image/
+
+        }
+
+       
+        for (int i = 0; i < n; i++) {
+            int left = 0;
+            int right = n - 1;
+
+            }
+                matrix[j][i] = temp;
+                matrix[i][j] = matrix[j][i];
+                int temp = matrix[i][j];
+            for (int j = i + 1; j < n; j++) {
+        for (int i = 0; i < n; i++) {
+       
+
+        int n = matrix.length;
+    public void rotate(int[][] matrix) {
+class Solution {
