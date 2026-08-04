6fe68ad046d58550d7ac397bad34d83ac5c76d83
Paritosh Anand <paritoshyadav809274@gmail.com>
2026-08-04 17:01:43 +0530

    Add solution for Find Missing Elements

---
+++ solutions/easy/Find Missing Elements.java
@@ -0,0 +1,19 @@
+// Title: Find Missing Elements
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/find-missing-elements/
+
+      ArrayList<Integer> list=new ArrayList<>();
+     int i=nums[0];
+     int j=nums[nums.length-1];
+      }
+     while(i<j){
+        if(!myset.contains(i)){
+     }
+            list.add(i);
+        }
+
+        i++;
+     return list;
+    }
+}
