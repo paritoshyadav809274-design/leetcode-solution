83dfd1232bcef23498f073dda7b3b8fd1385e277
Paritosh Anand <paritoshyadav809274@gmail.com>
2026-08-06 22:10:52 +0530

    Add solution for Maximum Average Subarray I

--- solutions/easy/Maximum Average Subarray I.java
+++ solutions/easy/Maximum Average Subarray I.java
@@ -3,6 +3,8 @@
             // Language: Java
             // Link: https://leetcode.com/problems/maximum-average-subarray-i/
 
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
        int n=nums.length;
        double max=0;
        double value=0;
@@ -12,11 +14,9 @@
        value=max;
        for(int i=0;i<n-k;i++){
         value=value-nums[i]+nums[k+i];
-       }
         max=Math.max(value,max);
+       }
        return max/k;
 
     }
 }
-    public double findMaxAverage(int[] nums, int k) {
-class Solution {
