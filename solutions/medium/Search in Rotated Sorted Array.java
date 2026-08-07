2437d13a9fa6d6efa7dc7a979aa4d4d31bf1ac8d
Paritosh Anand <paritoshyadav809274@gmail.com>
2026-08-07 14:51:32 +0530

    Add solution for Search in Rotated Sorted Array

---
+++ solutions/medium/Search in Rotated Sorted Array.java
@@ -0,0 +1,24 @@
+// Title: Search in Rotated Sorted Array
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+            }
+            else{
+
+               else low=mid+1;
+               }
+
+                 high=mid-1;
+               if(target>=nums[low] && target<nums[mid]){
+            if(nums[low]<=nums[mid]){
+            } 
+            return mid; 
+            if(target==nums[mid]){
+                if(target>nums[mid] && target<=nums[high]){
+                   low=mid+1;
+                }
+            int mid=low+(high-low)/2;
+          while(low<=high){
+          int high=n-1;
+          int low=0;
