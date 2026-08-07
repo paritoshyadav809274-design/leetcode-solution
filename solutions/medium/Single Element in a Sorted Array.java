de89112a5f2eb445fa4ff8f5a30dd9d93ce59ee0
Paritosh Anand <paritoshyadav809274@gmail.com>
2026-08-07 19:35:40 +0530

    Add solution for Single Element in a Sorted Array

---
+++ solutions/medium/Single Element in a Sorted Array.java
@@ -0,0 +1,24 @@
+// Title: Single Element in a Sorted Array
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/single-element-in-a-sorted-array/
+
+                mid--;
+             }
+             if(nums[mid]==nums[mid+1]){
+                start=mid+2; 
+             }
+             else{
+                end=mid;
+             }
+
+            }
+            return nums[start];
+
+             if(mid%2==1){
+             int mid=start+(end-start)/2;
+         int end=nums.length-1; 
+            while(start<end){
+    public int singleNonDuplicate(int[] nums) {
+         int start=0;
+class Solution {
