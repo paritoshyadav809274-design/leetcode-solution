e604f2976889becd3f8bcdc6b9abb9866c7b013d
Paritosh Anand <paritoshyadav809274@gmail.com>
2026-08-10 16:26:50 +0530

    Add solution for Valid Anagram

--- solutions/easy/Valid Anagram.java
+++ solutions/easy/Valid Anagram.java
@@ -3,22 +3,3 @@
             // Language: Java
             // Link: https://leetcode.com/problems/valid-anagram/
 
-        }
-
-        int[] count = new int[26];
-
-        for (int i = 0; i < s.length(); i++) {
-            count[s.charAt(i) - 'a']++;
-            count[t.charAt(i) - 'a']--;
-        }
-
-        for (int i = 0; i < 26; i++) {
-            if (count[i] != 0) {
-                return false;
-            }
-        }
-            return false;
-        if (s.length() != t.length()) {
-    public boolean isAnagram(String s, String t) {
-
-class Solution {
