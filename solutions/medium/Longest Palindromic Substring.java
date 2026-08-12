33b94afcd65779a9b5a340b5a93f5d0ef96c8e1f
Paritosh Anand <paritoshyadav809274@gmail.com>
2026-08-12 13:05:47 +0530

    Add solution for Longest Palindromic Substring

---
+++ solutions/medium/Longest Palindromic Substring.java
@@ -0,0 +1,24 @@
+// Title: Longest Palindromic Substring
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/longest-palindromic-substring/
+
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+    private int expand(String s, int left, int right) {
+            if (len > end - start) {
+            int len = Math.max(len1, len2);
+            int len2 = expand(s, i, i + 1); 
+            int len1 = expand(s, i, i);     
+        for (int i = 0; i < s.length(); i++) {
+        int end = 0;
+        int start = 0;
+            return "";
+        }
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) {
+class Solution {
