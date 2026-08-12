d468a36d974819e5abf2a15e78411bd267979602
Paritosh Anand <paritoshyadav809274@gmail.com>
2026-08-12 13:02:15 +0530

    Add solution for Maximum Nesting Depth of the Parentheses

--- solutions/easy/Maximum Nesting Depth of the Parentheses.java
+++ solutions/easy/Maximum Nesting Depth of the Parentheses.java
@@ -3,19 +3,21 @@
             // Language: Java
             // Link: https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/
 
-class Solution {
-    public int maxDepth(String s) {
-       int depth=0;
-       int maxdepth=0;
-       for(char ch:s.toCharArray()){
-          if(ch=='('){
-            depth++;
-          }
-       }
-           maxdepth= Math.max(depth,maxdepth);
-          else if(ch==')'){
-            depth--;
-          }
+        int depth = 0;
+        int maxDepth = 0;
+
+        for (char ch : s.toCharArray()) {
+
+            if (ch == '(') {
+                depth++;
+                maxDepth = Math.max(maxDepth, depth);
+            }
+
+            else if (ch == ')') {
+                depth--;
+            }
+        }
+
+        return maxDepth;
     }
-       return maxdepth; 
 }
