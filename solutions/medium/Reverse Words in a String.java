f4bdc0911dda47ed18091a8893c80d7429fcc60c
Paritosh Anand <paritoshyadav809274@gmail.com>
2026-08-12 13:14:35 +0530

    Add solution for Reverse Words in a String

--- solutions/medium/Reverse Words in a String.java
+++ solutions/medium/Reverse Words in a String.java
@@ -5,14 +5,14 @@
 
 class Solution {
     public String reverseWords(String s) {
-       String[] words=s.trim().split("\\s+");
-       StringBuilder sb=new StringBuilder();
-       for(int i=words.length-1;i>=0;i--){
-        sb.append(words[i]);
+      String[] t=s.trim().split("\\s+");
+      StringBuilder sb=new StringBuilder();
+      for(int i=t.length-1;i>=0;i--){
+        sb.append(t[i]);
         if(i!=0){
-       }
+      }
             sb.append(" ");
         }
     }
-       return sb.toString();
+      return sb.toString();
 }
