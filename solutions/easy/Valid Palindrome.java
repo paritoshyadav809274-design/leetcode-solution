b2f04cb372c4eff0e812554e43c550c388cab950
Paritosh Anand <paritoshyadav809274@gmail.com>
2026-08-21 07:55:34 +0530

    Add solution for Valid Palindrome

---
+++ solutions/easy/Valid Palindrome.java
@@ -0,0 +1,26 @@
+// Title: Valid Palindrome
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/valid-palindrome/
+
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
