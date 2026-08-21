8cfa881a5d3e26c407b62a31627d1669d969c8a6
Paritosh Anand <paritoshyadav809274@gmail.com>
2026-08-21 21:12:31 +0530

    Add solution for Longest Common Prefix

--- solutions/easy/Longest Common Prefix.java
+++ solutions/easy/Longest Common Prefix.java
@@ -3,18 +3,22 @@
             // Language: Java
             // Link: https://leetcode.com/problems/longest-common-prefix/
 
+
+        String s = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+
+            while (!strs[i].startsWith(s)) {
+
+                s = s.substring(0, s.length() - 1);
+
+                if (s.length() == 0) {
+                    return "";
+                }
+            }
+        }
+
+        return s;
+    }
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        String s=strs[0];
-    for(int i=1;i<strs.length;i++){
-       String b=strs[i];
-       while(!b.startsWith(s)){
-        s=s.substring(0,s.length()-1);
-       }
-    }
-        if(s.length()==0){
-            return "";
-        }
-    }
-    return s;
-}
