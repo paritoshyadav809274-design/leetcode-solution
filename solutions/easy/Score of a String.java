ff32e566b44d87d024a5ba939ed95e99af416a8c
Paritosh Anand <paritoshyadav809274@gmail.com>
2026-09-02 20:17:59 +0530

    Add solution for Score of a String

---
+++ solutions/easy/Score of a String.java
@@ -0,0 +1,15 @@
+// Title: Score of a String
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/score-of-a-string/
+
+class Solution {
+    public int scoreOfString(String s) {
+       int sum=0;
+       for(int i=0;i<s.length()-1;i++){
+        int differ=Math.abs(s.charAt(i)-s.charAt(i+1));
+       }
+    }
+        sum+=differ;
+       return sum;
+}
