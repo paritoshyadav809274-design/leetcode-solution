dc6011abc8b7839f88e860d988e444f361afa9b7
Paritosh Anand <paritoshyadav809274@gmail.com>
2026-09-21 16:34:21 +0530

    Add solution for Reverse Degree of a String

---
+++ solutions/easy/Reverse Degree of a String.java
@@ -0,0 +1,17 @@
+// Title: Reverse Degree of a String
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/reverse-degree-of-a-string/
+
+class Solution {
+    public int reverseDegree(String s) {
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            int reverseValue = 'z' - ch + 1;
+            int position = i + 1;
+            sum += reverseValue * position;
+        }
+        return sum;
+    }
+}
